1e1d30d88708f1eb6d42471bbe1df51b1d8236ac
Mohit_Mourya <key25mkm@gmail.com>
2024-01-08 00:49:16 +0530

    BMI Calculator

---
+++ script.js
@@ -0,0 +1,95 @@
+
+  
+  
+//   // When the user clicks on <span> (x), close the modal
+//   span.onclick = function() {
+//     modal.style.display = "none";
+//   }
+  
+//   // When the user clicks anywhere outside of the modal, close it
+//   window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   }
+
+let age = document.getElementById("age");
+let height = document.getElementById("height");
+let weight = document.getElementById("weight");
+let male = document.getElementById("m");
+let female = document.getElementById("f");
+let form = document.getElementById("form");
+let resultArea = document.querySelector(".comment");
+
+modalContent = document.querySelector(".modal-content");
+modalText = document.querySelector("#modalText");
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+
+function calculate(){
+ 
+  if(age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)){
+    modal.style.display = "block";
+    modalText.innerHTML = `All fields are required!`;
+
+  }else{
+    countBmi();
+  }
+
+}
+
+
+function countBmi(){
+  var p = [age.value, height.value, weight.value];
+  if(male.checked){
+    p.push("male");
+  }else if(female.checked){
+    p.push("female");
+  }
+
+  var bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
+      
+  var result = '';
+  if(bmi<18.5){
+    result = 'Underweight';
+     }else if(18.5<=bmi&&bmi<=24.99){
+    result = 'Healthy';
+     }else if(25<=bmi&&bmi<=29.9){
+    result = 'Overweight';
+     }else if(30<=bmi&&bmi<=34.9){
+    result = 'Obese';
+     }else if(35<=bmi){
+    result = 'Extremely obese';
+     }
+
+
+
+resultArea.style.display = "block";
+document.querySelector(".comment").innerHTML = `You are <span id="comment">${result}</span>`;
+document.querySelector("#result").innerHTML = bmi.toFixed(2);
+
+}
+
+
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+window.addEventListener('click', (event)=>{
+  if (event.target == modal) {
+        modal.style.display = "none";
+      }
+
+}
+);
